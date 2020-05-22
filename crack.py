e8261af0c60374eb457479bda2414dbbf91c37aa
Hunter <64382105+htyper@users.noreply.github.com>
2020-05-22 05:39:43 +0500

    Add files via upload

---
+++ crack.py
@@ -0,0 +1,28 @@
+import hashlib
+
+flag = 0
+
+pass_hash = input("Enter md5 hash: ")
+
+wordlist = input("Enter file name: ")
+
+try:
+	pass_file = open (wordlist, "r")
+except:
+	print("No file found")
+	quit()
+
+for word in pass_file:
+	enc_wrd = word.encode('utf-8')
+	digest = hashlib.md5(enc_wrd.strip()).hexdigest()
+	if digest == pass_hash:
+		print("Password found")
+		print("Password is " + word)
+		flag = 1
+		break
+
+if flag == 0:
+	print("Password/Passphrase is not in the list")
+
+
+	
